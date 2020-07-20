c6b5ea683392772c873e0ec91368b2961157ea70
anthonycc2 <60799284+anthonycc2@users.noreply.github.com>
2020-07-20 00:02:44 -0300

    File update

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -12,8 +12,8 @@ public class Programa {
 		
 		Usuario maria = new Usuario("Maria");
 		Usuario joice = new Usuario("Joice");
-		Usuario joao = new Usuario("Joao");
-		Usuario jose = new Usuario("Jose");
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
 		Usuario carla = new Usuario("Carla");
 		Usuario marcelo = new Usuario("Marcelo");
 		
@@ -26,21 +26,21 @@ public class Programa {
 		
 		Date data1 = sdf.parse("21/06/2018 13:05:44");
 		Postagem post1 = new Postagem(data1, "Viajando para Nova Zelândia", "Vou para este país maravilhoso!", carla);
-		post1.darLike();
-		post1.darLike();
+		post1.receberLike(maria);
+		post1.receberLike(joice);
 		post1.adicionarComentario(c1);
 		post1.adicionarComentario(c2);
 		
 		Date data2 = sdf.parse("28/07/2018 23:14:19");
 		Postagem post2 = new Postagem(data2, "Boa noite, caras", "Vejo vocês amanhã.", marcelo);
-		post2.darLike();
-		post2.darLike();
+		post2.receberLike(joao);
+		post2.receberLike(jose);
 		post2.adicionarComentario(c3);
 		post2.adicionarComentario(c4);
 		
 		System.out.println("--- Postagem #1 ---");
 		System.out.println(post1.toString());
-		System.out.println("--- Postagem #2 ---");
+		System.out.println("\n--- Postagem #2 ---");
 		System.out.println(post2.toString());
 	}
 
